48fd894147f8e05583606e4ccd6f57e7d4af391a
Sura Rueda <suray610@gmail.com>
2025-04-29 17:05:42 -0500

    Actualización con bootstrap

--- MoneyManager/src/Components/Login.jsx
+++ MoneyManager/src/Components/Login.jsx
@@ -1,9 +1,33 @@
 import React from "react";
+import Imagen from "../assets/ImagenInicio.jpg"
+
 
 const Login = () => {
   return (
     <div className="container">
-      <h2 className="text-center">Hola mundo</h2>
+      <div className="row">
+
+        {/*Columna de formulario*/}
+        <div className="col-md-4">
+          <div className="padre">
+            <div className="card card-body">
+              <form>
+                <input type="text" placeholder='Ingresar Email' className="cajatexto"/>
+                <input type="password" placeholder="Ingresar contraseña" className="cajatexto"/>
+                <button>Registrarse</button>
+              </form>
+            </div>
+          </div>
+        </div> 
+
+        {/*Columna grande*/}
+        <div className="col-md-8">
+          <img src={Imagen} alt="" className=""/>
+          <h2>Adios</h2>
+        </div> 
+
+
+      </div>
     </div>
   );
 };
